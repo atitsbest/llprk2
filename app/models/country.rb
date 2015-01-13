ddc5925c21d321923d3944225275216275979dd4
atitsbest <atitsbest@gmail.com>
2015-01-13 08:07:45 +0000

    + Land darf nicht gelöscht werden, wenn es von einer Bestellung verwendet wird.

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,2 +1,18 @@
 class Country < ActiveRecord::Base
+    has_many :orders
+
+    # Sicherstellen, dass wir kein Land löschen, das bei einer
+    # Adresse vorkommt.
+    before_destroy :ensure_not_referenced_by_order
+
+    private
+
+    def ensure_not_referenced_by_order
+        if orders.empty?
+            return true
+        else
+            errors.add(:base, 'Order present')
+            return false
+        end
+    end
 end
