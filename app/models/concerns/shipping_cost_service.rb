369529adc1293080cbd6dde85be8100d66c8153d
atitsbest <atitsbest@gmail.com>
2015-01-14 10:47:26 +0000

    + Versandkosten können jetzt auch für LineItems und ein LAnd berechnet werden.

--- app/models/concerns/shipping_cost_service.rb
+++ app/models/concerns/shipping_cost_service.rb
@@ -2,13 +2,18 @@ class ShippingCostService
 
     # Liefert die Versandkosten für eine Bestellung.
     def self.costs_for_order(order)
+        self.costs_for_line_items(order.line_items, order.country_id)
+    end
+
+    # Berechnet Versandkosten für LineItems in das angegebene Land.
+    def self.costs_for_line_items(line_items, country_id)
         result = 0.0
         base = 0.0
 
         # Für jedes Produkt die Versandkosten ermitteln.
-        order.line_items.each do |li|
+        line_items.each do |li|
             cost = ShippingCost.find_by(
-                country: order.country, 
+                country_id: country_id, 
                 shipping_category: li.product.shipping_category)
 
             if cost != nil
--- test/models/shipping_cost_service_test.rb
+++ test/models/shipping_cost_service_test.rb
@@ -1,16 +1,31 @@
 require 'test_helper'
 
 class ShippingCostServiceTest < ActiveSupport::TestCase
-  test "cost calculation for order" do
-    costs = ShippingCostService.costs_for_order(orders(:one))
-    assert_equal 4.5+2.5, costs
+    test "cost calculation for order" do
+        costs = ShippingCostService.costs_for_order(orders(:one))
+        assert_equal 4.5+2.5, costs
 
-    # Leerer Warenkorb.
-    costs = ShippingCostService.costs_for_order(orders(:no_line_items))
-    assert_equal 0.0, costs
+        # Leerer Warenkorb.
+        costs = ShippingCostService.costs_for_order(orders(:no_line_items))
+        assert_equal 0.0, costs
 
-    costs = ShippingCostService.costs_for_order(orders(:two))
-    assert_equal 4.5+(2*1.5), costs
-  end
+        costs = ShippingCostService.costs_for_order(orders(:two))
+        assert_equal 4.5+(2*1.5), costs
+    end
+
+    test "cost calculation for line items and country" do
+        order = orders(:one)
+        costs = ShippingCostService.costs_for_line_items(order.line_items, 'at')
+        assert_equal 4.5+2.5, costs
+
+        # Leerer Warenkorb.
+        order = orders(:no_line_items)
+        costs = ShippingCostService.costs_for_line_items(order.line_items, 'at')
+        assert_equal 0.0, costs
+
+        order = orders(:two)
+        costs = ShippingCostService.costs_for_line_items(order.line_items, 'at')
+        assert_equal 4.5+(2*1.5), costs
+      end
 end
 
