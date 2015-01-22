82c83f1ca49e3cfe591590c480ea685da1c337f7
atitsbest <atitsbest@gmail.com>
2015-01-22 15:33:39 +0000

    + Bilder werden mit Produkten gelöscht.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
     has_many :line_items
-    has_many :product_images
+    has_many :product_images, dependent: :destroy
     belongs_to :shipping_category
 
     validates :title, :description, presence: true
