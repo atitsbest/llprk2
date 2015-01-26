ed1a18c7719a75c07404d74a0d0b8845334e304f
atitsbest <atitsbest@gmail.com>
2015-01-26 11:31:29 +0000

    + Paperclip Bulkprocessing Optimierung hinzugefügt.

--- app/models/product_image.rb
+++ app/models/product_image.rb
@@ -4,7 +4,15 @@ class ProductImage < ActiveRecord::Base
             :retina => "800x800",
             :normal => "400x400",
             :gallery => "200x200"
-        }
+        },
+        :convert_options => {
+            :retina => "-quality 50",
+            :gallery => "-quality 75 -strip" 
+        },
+        processors: [:bulk]
+
+    after_post_process -> { Paperclip::BulkQueue.process(content) }
+
     validates_attachment :content,
         :presence => true,
         :content_type => { :content_type => /\Aimage\/.*\Z/ },
