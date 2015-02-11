bdb2293adce8da18cfb908e71c42c0018837ca3a
atitsbest <atitsbest@gmail.com>
2015-02-11 09:26:09 +0000

    + Mengen können mit +/- gesteuert werden.

--- app/assets/javascripts/cart.js
+++ app/assets/javascripts/cart.js
@@ -1,20 +1,34 @@
+// cart/show
 $('.cart.show').ready(function() {
     function ShowViewModel() {
         var self = this;
 
+        // Die Bestellzeilen.
         self.lineItems = ko.observableArray();
 
+        // Zwischensumme (ohne Versandkosten) für den Warenkorb.
         self.subTotal = ko.pureComputed(function() {
             return _.reduce(self.lineItems(), function(m, x) { return m + x.qty() * x.price; }, 0);
         });
 
+
+        // Warenkorbdaten vom Server laden.
         $.when($.getJSON('cart')).then(function(data) {
             self.lineItems(_(data.line_items).map(function(li) {
                 return _.extend(li, {
+                    // Die Anzahl muss überwacht werden.
                     qty: ko.observable(li.qty),
+                    // Zwischensumme dieser Bestellzeile.
                     sum: ko.pureComputed(function(){
                         return this.price * this.qty();
-                    }, li)
+                    }, li),
+                    incQty: function(){
+                       this.qty(this.qty() + 1);
+                    }.bind(li),
+                    decQty: function(){
+                        this.qty(Math.max(1, this.qty() - 1));
+                    }.bind(li),
+
                 });
             }));
         })
