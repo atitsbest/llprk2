ba1a3033d3b31e941cc9e5afecf3aac0fe3faa50
atitsbest <atitsbest@gmail.com>
2015-06-17 20:53:28 +0200

    + Admin-Design wird immer besser.

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,6 @@ module Railsllprk
     config.i18n.default_locale = :de
 
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
   end
 end
